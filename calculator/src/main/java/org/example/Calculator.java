3058bca2d814178a7c59459b26d981770cc7427e
santiago-salazar02 <santiago.salazar02@globant.com>
2024-08-08 19:57:57 -0500

    Feature(Advanced calculator): Mult and div

--- calculator/src/main/java/org/example/Calculator.java
+++ calculator/src/main/java/org/example/Calculator.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Calculator {
+    public double mult(double m1,double m2){
+        return m1*m2;
+    }
+    public double div(double m1,double m2){
+        return m1/m2;
+    }
 }
