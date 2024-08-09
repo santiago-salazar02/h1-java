05f6454917dc91fd920344679a0cb02224234ccc
santiago-salazar02 <santiago.salazar02@globant.com>
2024-08-08 19:58:58 -0500

    Feature(Sum and sub)

--- calculator/src/main/java/org/example/Calculator.java
+++ calculator/src/main/java/org/example/Calculator.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Calculator {
+    public double sum(double m1,double m2) {
+        return m1 + m2;
+    }
+    public double sub(double m1,double m2){
+        return m1-m2;
+    }
 }
